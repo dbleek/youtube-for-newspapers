b856285f3d47bcb4150832bc0dbabbec57546509
Oz Bejerano <ob2205@nyu.edu>
2024-04-21 11:30:22 -0400

    Add scripts to start and stop MongoDB

---
+++ start_mongo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export MONGODB_PORT="27040"
+
+mkdir -p /scratch/$USER/personal/db
+mkdir -p /scratch/$USER/personal/ip
+touch /scratch/$USER/personal/ip/ip_address.txt
+
+# start the database container
+singularity instance start --bind /scratch/ob2205/personal/db:/data/db /scratch/work/public/singularity/mongo-7.0.4.sif mongo-db-$USER
+
+# execute a command within container
+#  print IP address to file
+singularity exec instance://mongo-db-$USER echo $(hostname -i):$MONGODB_PORT > /scratch/$USER/personal/ip/ip_address.txt
+# start mongodb
+singularity exec instance://mongo-db-$USER /usr/bin/mongod --bind_ip_all --port $MONGODB_PORT &
---
+++ stop_mongo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# stop mongodb
+singularity instance stop mongo-db-$USER
